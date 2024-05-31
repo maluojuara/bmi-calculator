1c896577e22627b0124768b8a6ec79c62c1d1997
maluojuara <araujomaluc@gmail.com>
2024-05-31 11:44:49 -0300

    implementing imperial units logic

---
+++ scripts/calculateImperial.js
@@ -0,0 +1,86 @@
+// Utils
+
+function calculateBmi(feet, inches, stones, lbs) {
+    const height = (feet * 12) + inches;
+    const weight = (stones * 14) + lbs;
+
+
+    const bmi = (weight / (height ** 2)) * 703;
+
+    return bmi;
+}
+
+function describeBmiRange (bmi) {
+    let result;
+
+    if (bmi < 18.5)
+        result = 'underweight';
+
+    else if (bmi >= 18.5 && bmi < 25)
+        result = 'healthy weight';
+
+    else if (bmi >= 25 && bmi < 30)
+        result = 'overweight';
+
+    else if (bmi >= 30)
+        result = 'obese';
+
+    return result;
+}
+
+function calculateIdealWeight(feet, inches) {
+    const height = (feet * 12) + inches;
+
+    const maxWeight = 24.9 * (height ** 2);
+    const minWeight = 18.5 * (height ** 2);
+
+    const minStones = Math.floor(minWeight / 14);
+    const minPounds = Math.round(minWeight % 14);
+    const maxStones = Math.floor(maxWeight / 14);
+    const maxPounds = Math.round(maxWeight % 14);
+
+    const idealWeight = `${minStones}st ${minPounds}lbs - ${maxStones}st ${maxPounds}lbs`;
+
+    return idealWeight;
+}
+
+
+// DOM manipulation
+
+function displayBmi (feet, inches, stones, lbs) {
+    const result = document.querySelector('.box__result');
+    const bmi = calculateBmi(feet, inches, stones, lbs);
+    const bmiRange = describeBmiRange(bmi);
+    const idealWeight = calculateIdealWeight(feet, inches);
+
+    result.innerHTML = `
+        <div class="box__result__container">
+            <p class="result-intro">Your BMI is...</p>
+            <p class="result">${bmi.toFixed(1)}</p>
+        </div>
+
+        <div class="box__result__container">
+            <p class="result-description">Your BMI suggests you're a ${bmiRange}. Your ideal weight is between <span>${idealWeight}</span></p>
+        </div>`;
+
+    result.style.flexDirection = 'row';
+}
+
+// Input
+
+function handleInput() {
+
+    const feetInput = parseFloat(document.querySelector('#height-ft').value) || 0;
+    const inchesInput = parseFloat(document.querySelector('#height-in').value) || 0;
+    const stonesInput = parseFloat(document.querySelector('#weight-st').value) || 0;
+    const lbsInput = parseFloat(document.querySelector('#weight-lbs').value) || 0;
+   
+    displayBmi(feetInput, inchesInput, stonesInput, lbsInput);
+}
+
+document.querySelector('#height-ft').addEventListener('input', handleInput);
+document.querySelector('#height-in').addEventListener('input', handleInput);
+document.querySelector('#weight-st').addEventListener('input', handleInput);
+document.querySelector('#weight-lbs').addEventListener('input', handleInput);
+
+
--- scripts/calculateMetric.js
+++ scripts/calculateMetric.js
@@ -1,4 +1,9 @@
-const result = document.querySelector('.box__result');
+// Utils
+
+function calculateBmi(height, weight) {
+    const bmi = weight / (height ** 2);
+    return bmi;
+}
 
 function describeBmiRange (bmi) {
     let result;
@@ -6,10 +11,10 @@ function describeBmiRange (bmi) {
     if (bmi < 18.5)
         result = 'underweight';
 
-    else if (18.5 >= bmi > 25)
+    else if (bmi >= 18.5 && bmi < 25)
         result = 'healthy weight';
 
-    else if (25 >= bmi > 30)
+    else if (bmi >= 25 && bmi < 30)
         result = 'overweight';
 
     else if (bmi >= 30)
@@ -18,94 +23,85 @@ function describeBmiRange (bmi) {
     return result;
 }
 
-function calculateIdealWeight(height, weight) {
+function calculateIdealWeight(height) {
+    const maxWeight = 24.9 * (height ** 2);
+    const minWeight = 18.5 * (height ** 2);
+
+    const idealWeight = `${minWeight.toFixed(1)}kgs - ${maxWeight.toFixed(1)}kgs.`;
+
+    return idealWeight;
+}
+
+// DOM manipulation
+
+function displayBmi (height, weight) {
+    const result = document.querySelector('.box__result');
     const bmi = calculateBmi(height, weight);
+    const bmiRange = describeBmiRange(bmi);
+    const idealWeight = calculateIdealWeight(height);
 
-    
-}
-
-function calculateBmi(height, weight) {
-    const bmi = weight / (height ** 2);
-    return bmi;
-    // const bmiRange = describeBmiRange(bmi);
-    // result.innerHTML = `
-    // <div class="box__result__container">
-    //     <p class="result-intro">Your BMI is...</p>
-    //     <p class="result">${bmi.toFixed(2)}</p>
-    // </div>
-
-    // <div class="box__result__container">
-    //     <p class="result-description">Your BMI suggests you're a ${bmiRange}. Your ideal weight is between <span>63.3kgs - 85.2kgs.</span></p>
-    // </div>`;
-
-    // result.style.flexDirection = 'row';
-}
-
-
-
-
-
-
-
-function displayBmi(height, weight) {
-    const bmi = (height > 0 && weight > 0) ? (weight / (height ** 2)) : 0;
     result.innerHTML = `
-    <div class="box__result__container">
-        <p class="result-intro">Your BMI is...</p>
-        <p class="result">${bmi.toFixed(2)}</p>
-    </div>
+        <div class="box__result__container">
+            <p class="result-intro">Your BMI is...</p>
+            <p class="result">${bmi.toFixed(1)}</p>
+        </div>
 
-    <div class="box__result__container">
-        <p class="result-description">Your BMI suggests you're a ${bmiRange}. Your ideal weight is between <span>63.3kgs - 85.2kgs.</span></p>
-    </div>`;
+        <div class="box__result__container">
+            <p class="result-description">Your BMI suggests you're a ${bmiRange}. Your ideal weight is between <span>${idealWeight}</span></p>
+        </div>`;
+
     result.style.flexDirection = 'row';
 }
 
-function showError(message) {
-    result.innerHTML = `
-    <div class="box__result__container">
-        <p class="result-error">${message}</p>
-    </div>`;
-    result.style.flexDirection = 'column';
-}
+// Input
 
-function handleInputMetric() {
+function handleInput() {
     const heightMetricInput = parseFloat(document.querySelector('#height-cm').value) / 100 || 0;
     const weightMetricInput = parseFloat(document.querySelector('#weight-kg').value) || 0;
-
+   
     displayBmi(heightMetricInput, weightMetricInput);
 }
 
-function handleBlurMetric() {
-    const heightMetricInput = parseFloat(document.querySelector('#height-cm').value) / 100;
-    const weightMetricInput = parseFloat(document.querySelector('#weight-kg').value);
+document.querySelector('#height-cm').addEventListener('input', handleInput);
+document.querySelector('#weight-kg').addEventListener('input', handleInput);
 
-    if (!isNaN(heightMetricInput) && heightMetricInput > 0 && !isNaN(weightMetricInput) && weightMetricInput > 0) {
-        calculateBmi(heightMetricInput, weightMetricInput);
-    } else {
-        showError('Please enter valid height and weight values.');
-    }
-}
+// function showError(message) {
+//     result.innerHTML = `
+//     <div class="box__result__container">
+//         <p class="result-error">${message}</p>
+//     </div>`;
+//     result.style.flexDirection = 'column';
+// }
+x
 
-let heightCompleted = false;
-let weightCompleted = false;
 
-function handleHeightBlur() {
-    heightCompleted = true;
-    if (heightCompleted && weightCompleted) {
-        handleBlurMetric();
-    }
-}
+// function handleBlurMetric() {
+//     const heightMetricInput = parseFloat(document.querySelector('#height-cm').value) / 100;
+//     const weightMetricInput = parseFloat(document.querySelector('#weight-kg').value);
 
-function handleWeightBlur() {
-    weightCompleted = true;
-    if (heightCompleted && weightCompleted) {
-        handleBlurMetric();
-    }
-}
+//     if (!isNaN(heightMetricInput) && heightMetricInput > 0 && !isNaN(weightMetricInput) && weightMetricInput > 0) {
+//         calculateBmi(heightMetricInput, weightMetricInput);
+//     } else {
+//         showError('Please enter valid height and weight values.');
+//     }
+// }
 
-document.querySelector('#height-cm').addEventListener('input', handleInputMetric);
-document.querySelector('#weight-kg').addEventListener('input', handleInputMetric);
+// let heightCompleted = false;
+// let weightCompleted = false;
 
-document.querySelector('#height-cm').addEventListener('blur', handleHeightBlur);
-document.querySelector('#weight-kg').addEventListener('blur', handleWeightBlur);
+// function handleHeightBlur() {
+//     heightCompleted = true;
+//     if (heightCompleted && weightCompleted) {
+//         handleBlurMetric();
+//     }
+// }
+
+// function handleWeightBlur() {
+//     weightCompleted = true;
+//     if (heightCompleted && weightCompleted) {
+//         handleBlurMetric();
+//     }
+// }
+
+// document.querySelector('#height-cm').addEventListener('blur', handleHeightBlur);
+// document.querySelector('#weight-kg').addEventListener('blur', handleWeightBlur);
